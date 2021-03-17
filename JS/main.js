5bde8b052dc4ddbce294c9eab4ff6766ffe5cd04
PranayBajracharya <law.pran99@gmail.com>
2021-03-17 20:55:58 +0545

    Initial commit

---
+++ JS/main.js
@@ -0,0 +1,16 @@
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function (event) {
+    if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top
+            },
+            600
+        );
+    }
+});
+
